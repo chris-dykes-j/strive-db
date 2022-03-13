87eb4f61b3a9403187594f671458ccdf5936178e
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-03-13 12:02:33 -0400

    Add character query

--- server/index.js
+++ server/index.js
@@ -11,17 +11,36 @@ const pool = new pg({
 pool.connect();
 app.use(express.json());
 
-app.get("/", async (req, res) => {
+// Default homepage. Getting data for Ram right now, but should be fixed later.
+app.get("/", async (_, res) => {
 	try {
 		const ram_query = await pool.query("SELECT * FROM move_list");
-		console.log(ram_query);
+		// console.log(ram_query);
 		res.send(ram_query);
 	}
 	catch (err) {
 		if (err) { console.log(err); }
 	}
-
 });
+
+app.get("/:character", async (req, res) => {
+	console.log(req.params.id);
+	try {
+		const move_query = await pool.query(
+			`SELECT characters.character_name, move_list.*
+			FROM characters
+			JOIN move_list
+			ON characters.character_id = move_list.character_id
+			
+			ORDER BY move_id;`);
+		// How to get :character? Below not working.
+		// WHERE characters.character_name LIKE '%${req.params.id}%'
+		res.send(move_query);
+	}
+	catch (err) {
+		if (err) { console.log(err); }
+	}
+})
 
 app.listen(PORT, () => {
 	console.log(`Heaven or Hell. Port: ${PORT}`);
