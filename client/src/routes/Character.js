2004294063ffade7102408715ea55b228bc558f0
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-03-14 12:35:26 -0400

    Fetch JSON from server

--- client/src/routes/Character.js
+++ client/src/routes/Character.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from './components/Header';
+import Image from './components/Image';
 import MoveList from './components/MoveList';
 
 const Character = () => {
+	fetch("http://localhost:8000/ramlethal_valentine")
+		.then(response => response.json())
+		.then((jsonData) => {
+			console.log(jsonData)
+		})
+		.catch((error) => {
+			console.error(error)
+		});
 	return (
 		<div>
 			<Header title="Ramlethal" />
+			<Image />
 			<MoveList />
 		</div>
 	)
---
+++ client/src/routes/components/Image.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ram from "../../images/RamIdle.gif";
+
+const Image = () => {
+	return (
+		<img src={ram} alt="ram-idle-animation" />
+	)
+}
+
+export default Image;
