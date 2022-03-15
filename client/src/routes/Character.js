94cefdd5563880385b3f7f64c959dc71524ada34
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-03-15 17:21:48 -0400

    Polish client side

--- client/src/routes/Character.js
+++ client/src/routes/Character.js
@@ -4,14 +4,6 @@ import Image from './components/Image';
 import MoveList from './components/MoveList';
 
 const Character = () => {
-	let attacks = [];
-	fetch("http://localhost:8000/ramlethal_valentine")
-		.then(response => response.json())
-		.then((attackData) => {
-			attackData.forEach(element => attacks.push(element["move_name"]))
-		})
-		.catch((error) => console.error(error));
-	console.log(attacks);
 	return (
 		<div>
 			<Header title="Ramlethal" />
---
+++ client/src/routes/components/MoveData.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MoveData = () => {
+	return (
+		<div>MoveData</div>
+	)
+}
+
+export default MoveData
